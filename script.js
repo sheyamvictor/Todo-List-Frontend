8394864352e35230966277f8c067a2c486ce6eb7
sheyamvictor <sheyamjoseph@gmail.com>
2024-10-20 16:31:52 +0530

    updated

--- script.js
+++ script.js
@@ -10,14 +10,26 @@ window.onload = () => {
     todos = JSON.parse(localStorage.getItem('todos')) || []
 
     todos.forEach(todo => addTodo(todo));
+    
 }
 
-btn.addEventListener('click', () => {
-    todos.push(inputBox.value)
-    localStorage.setItem('todos', JSON.stringify(todos))
 
-    addTodo(inputBox.value)
-    inputBox.value = ''
+
+btn.addEventListener('click', () => {
+
+    if(inputBox.value.length >0){
+        todos.push(inputBox.value)
+        localStorage.setItem('todos', JSON.stringify(todos))
+    
+        addTodo(inputBox.value)
+        inputBox.value = ''
+
+        inputBox.focus()
+
+    }else{
+        alert("Enter the todo")
+    }
+  
 
 })
 
@@ -28,22 +40,32 @@ function addTodo(todo) {
     let list = document.createElement('li');
     list.innerText = todo
     listContainer.appendChild(list)
+
     list.addEventListener('click', () => {
+    
         list.style.textDecoration = 'line-through black'
         remove(todo)
     })
 
     list.addEventListener('dbClick', () => {
         listContainer.removeChild(list)
-        remove(todo)
+        list.remove(todo)
     })
 }
 
 function remove(todo) {
     let index = todos.indexOf(todo)
+
     if (index > -1) {
-        todos.splice(index, 1)
+        todos.splice(index, 1)  
     }
+
+    else if('dbClick'){
+    window.location.reload(listContainer)
+        
+    }
+  
+    
     localStorage.setItem('todos', JSON.stringify(todos))
 
 }
